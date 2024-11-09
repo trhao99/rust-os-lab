93127c32d1fb0364edf41eb6646d3778d12f9665
trhao <1927978923@qq.com>
2024-11-09 23:21:13 +0800

    lab1: freestanding-rust-binary 独立可执行程序

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
